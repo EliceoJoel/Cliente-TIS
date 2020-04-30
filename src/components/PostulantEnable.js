431a288373e048b75c4cdc71f2a61776f290258c
marcos27SG <marcosrsg27@gmail.com>
2020-04-30 14:42:13 -0400

    aumentando post en buscar postulante

--- src/components/PostulantEnable.js
+++ src/components/PostulantEnable.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import {getAnnouncement} from './UserFunctions'
 import Select from 'react-select'
+import axios from 'axios'
+import {getAnnouncementID} from './UserFunctions'
 var conv  = [] 
+//var postulantenable = []
 export class PostulantEnable extends Component {
     constructor(props) {
         super(props)
     
         this.state = {
-             codSis:''
+             codSis:'' ,
+             conv: ''
         }
     }
     
@@ -23,21 +27,55 @@ export class PostulantEnable extends Component {
                 conv[i] = object
               }
         })
+        
     }
+
+    
     selectConvChange = selectedConvOption =>{
-        //this.setState({selectedConvOption_error:''})
-        //this.setState({selectedConvOption},()=>this.fillAuxiliary())
+       
         this.setState({selectedOptionConv:selectedConvOption})
-        //this.setState({showAux:true})
-        //this.setState({selectedOptionAux:null})
-        //delete auxiliary array data
-        //var array = []
-        //aux = array
+        //this.setState({[selectedop.target.name]: e.target.value })
+       // this.setState ({[selectedConvOption.name]: selectedConvOption.value })
+       
+    }
+    // getPostulantEnable() {
+    //     axios.get('/api/postulantenable')
+    //     .then(response => {
+    //         this.setState({postulantenable : response.data});
+    //     })
+    //     .catch(e => {
+    //         console.log(e);
+    //     })
+    //   }
+
+      handleSearch(e ){
+        e.preventDefault()
+        let codSis = this.state.codSis
+        let conv =  this.state.selectedOptionConv.label
+        let send = new FormData()
+            
+            send.append('codSis', codSis)
+            send.append('conv', conv )
+            axios({
+               method: 'post',
+               url: 'api/postulantEnableSearch',
+               data: send,
+               headers: {'Content-Type': 'multipart/form-data' }
+               }).then(response =>{
+                console.log(response)
+            }) 
+            .catch(error => {
+                console.log(error)
+            })
+           
+
+    
     }
 
     render() {
         const {codSis} = this.state
         const { selectedOptionConv } = this.state
+        //this.getPostulantEnable()
         return (
             <div className="justify-content-center">
            
@@ -72,9 +110,16 @@ export class PostulantEnable extends Component {
  
                         </div>
                         <div>
-                        <button className="btn btn-outline-info mt-4" variant="warning" onClick ={(AuxEvent) => this.handleAux(AuxEvent)} >Buscar Postulante</button>
+                        <button className="btn btn-outline-info mt-4" variant="warning" onClick ={(e) => this.handleSearch(e)} >Buscar Postulante</button>
 
                         </div>
+                        {/* <div>
+                              {postulantenable.map(enable =>
+                                <div key = {enable.id}>
+                                     <label htmlFor="cod_sys">cod_sys : {enable.sys_code},</label>
+                                     <label htmlFor="auxiliary">auxiliatura : {enable.auxiliary}</label>
+                                 </div>   )}
+                        </div> */}
                       </div>
 
                 </div>
