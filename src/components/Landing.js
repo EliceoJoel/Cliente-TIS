64d52d66bcb5568a80bca2d5667023b3455016ad
marcos27SG <marcosrsg27@gmail.com>
2020-05-04 18:47:45 -0400

    las fechas importantes se muestran en landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,15 +1,59 @@
 import React, { Component } from 'react'
-
+import axios from 'axios'
 class Landing extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         arreglo:[]
+      }
+    }
+    componentDidMount() {
+      axios.get('/api/importantDate')
+      .then(response => {
+          this.setState({arreglo : response.data});
+      })
+      .catch(e => {
+          console.log(e);
+      })
+    }
+
   render() {
     return (
       <div className="container">
         <div className="jumbotron mt-5">
-          <div className="col-sm-8 mx-auto">
+          <main 
+            
+          >
             <h1 className="text-center">WELCOME</h1>
+          </main>
+          
+        
+        <section>
+
+          <div className="col-md-12" >
+            <h2>AVISO DE FECHAS</h2>
+          {this.state.arreglo.map(aviso =>
+                    <div >
+                                 
+                                  <h5 className="col-md-10 " htmlFor="Nombre">{aviso.event}</h5>
+                                  <label className="col-md-5 " htmlFor="Nombre">{aviso.description}</label> 
+                                  <label className="col-md-8 " htmlFor="Nombre">{aviso.date}</label>
+                                 
+                                  <br></br>
+                                  </div>
+                                   )}
+                                 
+                             
+                         
+         
           </div>
-        </div>
+         
+        </section>
+        
+          </div>
       </div>
+      
     )
   }
 }
